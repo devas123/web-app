8f74d76d3ef4ee86b292c5fb1d47f2166bb6dba9
ggrigori <grigoriy.grigoriev@gmail.com>
2022-06-02 22:09:43 +0100

    started working on schedule

--- apps/compmanager-frontend/src/app/pipes/display-category.pipe.ts
+++ apps/compmanager-frontend/src/app/pipes/display-category.pipe.ts
@@ -1,17 +1,13 @@
 import {Pipe, PipeTransform} from '@angular/core';
 import {displayCategory} from '../commons/model/competition.model';
-import {CategoryDescriptor} from "@frontend-nx/protobuf";
+import {CategoryState} from "@frontend-nx/protobuf";
 
 @Pipe({
   name: 'displayCategory'
 })
 export class DisplayCategoryPipe implements PipeTransform {
-  transform(value?: CategoryDescriptor): string {
-    if (value) {
-      return displayCategory(value, 20);
-    } else {
-      return '';
-    }
+  transform(value?: CategoryState): string {
+    return displayCategory(value?.category, 20);
   }
 }
 
