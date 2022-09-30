efec8e2981ff4f50d2419198fb866f58693a43a7
ggrigori <grigoriy.grigoriev@gmail.com>
2022-09-30 21:52:58 +0100

    Updated proxy

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,22 +1,15 @@
 PROXY_CONFIG = [
   {
     context: [
-      "/accounts",
+      "/accountsrv",
       "/competition/command",
       "/competition/scommand",
+      "/query"
     ],
     target: "http://localhost:8080",
     secure: false,
     logLevel: "debug"
   },
-  {
-    context: [
-      "/query"
-    ],
-    target: "http://localhost:9000",
-    secure: false,
-    logLevel: "debug"
-  }
 ];
 
 module.exports = PROXY_CONFIG;
